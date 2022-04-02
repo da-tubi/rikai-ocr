3532ff530a25045d27024cb2a9d4ebcc8a2715b8
Darcy Shen <da@tubi.tv>
2022-04-02 14:22:41 +0800

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Rikai OCR
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_namespace_packages, setup
+import pathlib
+
+with open(
+    pathlib.Path(__file__).absolute().parent / "README.md", "r"
+) as fh:
+    long_description = fh.read()
+
+setup(
+    name="rikai-ocr",
+    version="0.0.1",
+    license="Apache License, Version 2.0",
+    author="Darcy Shen",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/da-tubi/rikai-ocr",
+    python_requires=">=3.7",
+    install_requires=["rikai>=0.1.5"],
+    extras_require={
+        "dev": [
+            "black",
+            "isort",
+            # for testing
+            "pytest",
+        ]
+    },
+    packages=find_namespace_packages(include=["rikai.*"]),
+)
